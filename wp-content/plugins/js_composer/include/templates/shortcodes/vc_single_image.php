267c33e4c20a46f468978470ce73798584969134
Passlack, Leif <Passlack@ideal-versicherung.de>
2021-09-06 13:41:16 +0200

    WPBakery Page Builder Pro Version

--- wp-content/plugins/js_composer/include/templates/shortcodes/vc_single_image.php
+++ wp-content/plugins/js_composer/include/templates/shortcodes/vc_single_image.php
@@ -76,7 +76,7 @@ switch ( $source ) {
 
 		$img = wpb_getImageBySize( array(
 			'attach_id' => $img_id,
-			'thumb_size' => $img_size,
+			'thumb_size' => strtolower( $img_size ),
 			'class' => 'vc_single_image-img',
 		) );
 
@@ -145,11 +145,11 @@ switch ( $onclick ) {
 		break;
 
 	case 'link_image':
-		wp_enqueue_script( 'prettyphoto' );
-		wp_enqueue_style( 'prettyphoto' );
+		wp_enqueue_script( 'lightbox2' );
+		wp_enqueue_style( 'lightbox2' );
 
-		$a_attrs['class'] = 'prettyphoto';
-		$a_attrs['data-rel'] = 'prettyPhoto[rel-' . get_the_ID() . '-' . wp_rand() . ']';
+		$a_attrs['class'] = '';
+		$a_attrs['data-lightbox'] = 'lightbox[rel-' . get_the_ID() . '-' . wp_rand() . ']';
 
 		// backward compatibility
 		if ( ! vc_has_class( 'prettyphoto', $el_class ) && 'external_link' === $source ) {
