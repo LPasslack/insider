23e2e6927c158beebe64b62762e43e0f03d3a6b5
Passlack, Leif <Passlack@ideal-versicherung.de>
2022-10-26 14:27:54 +0200

    Wordpress Plugins aus der GIT Versionierung entfernen

--- wp-content/plugins/magplus-amp/templates/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="amp-wp-footer">
-	<div class="amp-footer-copyright-wrapper">
-    <div class="footer-copy-text"><?php echo magplus_get_opt('amp-footer-copyright-text'); ?></div>
-	</div>
-</footer>
--- wp-content/plugins/magplus-amp/templates/searchform.php
+++
@@ -1,9 +0,0 @@
-<div class="tt-s-search">
-  <form role="search" class="search-form search clearfix <?php echo ! get_search_query( false ) ? 'empty' : ''; ?>" action="<?php echo esc_url( amp_get_site_url() ) ?>/" target="_top" novalidate>
-    <input type="text" required="" name="s" value="<?php the_search_query() ?>" placeholder="<?php echo magplus_get_opt('translation-search-article'); ?>">
-    <div class="tt-s-search-submit">
-      <i class="fa fa-search" aria-hidden="true"></i>
-      <input type="submit" value="">
-    </div>
-  </form>
-</div>
